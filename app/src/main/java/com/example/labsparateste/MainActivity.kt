8b49af06b8d7a71b47acb0637bfd5104531a7190
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2022-04-20 18:25:19 +0100

    lab controlos

--- app/src/main/java/com/example/labsparateste/MainActivity.kt
+++ app/src/main/java/com/example/labsparateste/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.labsparateste
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +17,14 @@ class MainActivity : AppCompatActivity() {
         //add method B
         
         //john added method c
+    }
+    fun button1(view: View) {
+        var editTextHello = findViewById<EditText>(R.id.editTextPersonName)
+        Toast.makeText(this,editTextHello.text.toString(),Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.txt1).setText(editTextHello.text)
+    }
+
+    fun button3(view: View) {
+
     }
 }
